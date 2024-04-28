8abbcadfe7da26d3758878ff0c2fd111ea113212
leoncana <103256170+leoncana@users.noreply.github.com>
2024-04-28 04:15:56 +0200

    Update page.tsx

--- moviebookingadmin/src/app/page.tsx
+++ moviebookingadmin/src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home page</h1>
+      <h1>ADMIN PAGE</h1>
     </main>
   )
 }
